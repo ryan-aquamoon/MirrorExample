8aa8cadd56a4ac0356b573f59a889b9a4b6f53bc
ryan-aquamoon <ryan.aquamoon@gmail.com>
2021-03-16 15:16:30 +0800

    My first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    void Start()
+    {
+        if (isLocalPlayer)
+            CmdRandomPos();
+    }
+
+    [Command]
+    void CmdRandomPos()
+    {
+        transform.position = new Vector2(Random.Range(-7.5f, 7.5f), Random.Range(-2.5f, 2.5f));
+        RpcRandomPos(transform.position);
+    }
+
+    [ClientRpc]
+    void RpcRandomPos(Vector2 pos)
+    {
+        transform.position = pos;
+    }
+
+}
